ced345d114ba8f27f7454bc69e41c794312c30bb
zhoumaotuo <zmtzawqlp@live.com>
2016-01-06 10:53:05 +0800

    Add UWP Toolkit solution

---
+++ src/MyUWPToolkit/MyUWPToolkit/Util/PlatformIndependent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUWPToolkit.Util
+{
+    public static class PlatformIndependent
+    {
+        /// <summary>
+        /// Indicates whether the running device is windows phone device.
+        /// </summary>
+        internal static bool IsWindowsPhoneDevice
+        {
+            get
+            {
+                if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
