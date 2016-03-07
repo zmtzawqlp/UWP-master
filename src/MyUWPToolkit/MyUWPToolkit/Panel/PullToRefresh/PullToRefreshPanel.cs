97405f5345aa7114bbb2e74ede5be93726cd623f
zhoumaotuo <zmtzawqlp@live.com>
2016-03-07 16:12:13 +0800

    fix PullToRefreshPanel issue that go back from other page, it will call refresh if the scroller has VerticalOffset

--- src/MyUWPToolkit/MyUWPToolkit/Panel/PullToRefresh/PullToRefreshPanel.cs
+++ src/MyUWPToolkit/MyUWPToolkit/Panel/PullToRefresh/PullToRefreshPanel.cs
@@ -153,7 +153,8 @@ namespace MyUWPToolkit
         {
             ScrollViewer sv = sender as ScrollViewer;
             IsReachThreshold = sv.VerticalOffset == 0;
-            if (IsReachThreshold)
+            //fix issue that go back from other page, it will call refresh if the scroller has VerticalOffset
+            if (IsReachThreshold && e.IsIntermediate)
             {
                 _scrollViewer.DirectManipulationCompleted += _scrollViewer_DirectManipulationCompleted;
             }
