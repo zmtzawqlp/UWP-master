99c9c0e2dedbe0c62788cc190339d3a000d47b68
zhoumaotuo <zmtzawqlp@live.com>
2016-06-15 09:44:27 +0800

    Add UWP.Chart

---
+++ src/UWP.Chart/UWP.Chart/Chart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace UWP.Chart
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Chart : Control, IDisposable
+    {
+        public Chart()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
