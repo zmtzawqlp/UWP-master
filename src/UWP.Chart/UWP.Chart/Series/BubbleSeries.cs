f3a6286cc3071e84e78c9c1c706470b155b9301a
zhoumaotuo <zmtzawqlp@live.com>
2016-07-08 17:59:39 +0800

    Add new classes

---
+++ src/UWP.Chart/UWP.Chart/Series/BubbleSeries.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace UWP.Chart.Series
+{
+    public class BubbleSeries : PointSeries
+    {
+        #region Public Property
+        /// <summary>
+        /// The binding used to identify the size value.
+        /// </summary>
+        private Binding _sizeValueBinding;
+
+        /// <summary>
+        /// Gets or sets the Binding to use for identifying the size of the bubble.
+        /// </summary>
+        public Binding SizeValueBinding
+        {
+            get
+            {
+                return _sizeValueBinding;
+            }
+            set
+            {
+                if (_sizeValueBinding != value)
+                {
+                    _sizeValueBinding = value;
+                    OnPropertyChanged("SizeValueBinding");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Binding Path to use for identifying the size of the bubble.
+        /// </summary>
+        public string SizeValuePath
+        {
+            get
+            {
+                return (null != SizeValueBinding) ? SizeValueBinding.Path.Path : null;
+            }
+            set
+            {
+                if (null == value)
+                {
+                    SizeValueBinding = null;
+                }
+                else
+                {
+                    SizeValueBinding = new Binding { Path = new PropertyPath(value) };
+                }
+            }
+        }
+        #endregion
+
+        #region DependencyProperty
+
+
+        public DoubleCollection SizeValues
+        {
+            get { return (DoubleCollection)GetValue(SizeValuesProperty); }
+            set { SetValue(SizeValuesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SizeValues.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SizeValuesProperty =
+            DependencyProperty.Register("SizeValues", typeof(DoubleCollection), typeof(BubbleSeries), new PropertyMetadata(null, OnSizeValuesChanged));
+
+        private static void OnSizeValuesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+        public IEnumerable SizeValuesSource
+        {
+            get { return (IEnumerable)GetValue(SizeValuesSourceProperty); }
+            set { SetValue(SizeValuesSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SizeValuesSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SizeValuesSourceProperty =
+            DependencyProperty.Register("SizeValuesSource", typeof(IEnumerable), typeof(BubbleSeries), new PropertyMetadata(null, OnSizeValuesSourceChanged));
+
+        private static void OnSizeValuesSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        #endregion
+    }
+}
---
+++ src/UWP.Chart/UWP.Chart/Series/Series.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWP.Chart.Common;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace UWP.Chart.Series
+{
+    public class Series : BindableBase
+    {
+
+        #region Public Property
+        private Binding _dependentValueBinding;
+        /// <summary>
+        /// The binding used to identify the dependent value binding.
+        /// </summary>
+        public Binding DependentValueBinding
+        {
+            get
+            {
+                return _dependentValueBinding;
+            }
+            set
+            {
+                if (_dependentValueBinding != value)
+                {
+                    _dependentValueBinding = value;
+                    OnPropertyChanged("DependentValueBinding");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Binding Path to use for identifying the dependent value.
+        /// </summary>
+        public string DependentValuePath
+        {
+            get
+            {
+                return (null != DependentValueBinding) ? DependentValueBinding.Path.Path : null;
+            }
+            set
+            {
+                if (null == value)
+                {
+                    DependentValueBinding = null;
+                }
+                else
+                {
+                    DependentValueBinding = new Binding() { Path = new PropertyPath(value) };
+                }
+            }
+        }
+
+
+        #endregion
+
+        #region DependencyProperty
+        /// <summary>
+        /// Gets or sets the values(Y) collection.
+        /// </summary>
+        public DoubleCollection DependentValues
+        {
+            get { return (DoubleCollection)GetValue(DependentValuesProperty); }
+            set { SetValue(DependentValuesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DependentValues.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DependentValuesProperty =
+            DependencyProperty.Register("DependentValues", typeof(DoubleCollection), typeof(Series), new PropertyMetadata(null, OnDependentValuesChanged));
+
+        private static void OnDependentValuesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Gets or sets the dependent values(Y) source.
+        /// </summary>
+        public IEnumerable DependentValuesSource
+        {
+            get { return (IEnumerable)GetValue(DependentValuesSourceProperty); }
+            set { SetValue(DependentValuesSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DependentValuesSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DependentValuesSourceProperty =
+            DependencyProperty.Register("DependentValuesSource", typeof(IEnumerable), typeof(Series), new PropertyMetadata(null, OnDependentValuesSourceChanged));
+
+        private static void OnDependentValuesSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
